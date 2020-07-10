c8edbd57f7584d4e1b4601d462bebc31a9c5f9cf
Abbta <abbe.tanndal@gmail.com>
2020-07-10 11:19:21 +0200

    begin adding svg via js

--- graph.js
+++ graph.js
@@ -1,10 +1,27 @@
 document.onload = function () {
-    var graph = this.document.getElementById("graph");
+
 }
-graph.onload = function () {
-    var ctx = graph.getContext("2d");
-    ctx.beginPath();
-    ctx.moveTo(0, 30);
-    ctx.lineTo(700, 30);
-    ctx.stroke();
-} 
+
+document.body.onload = function () {
+    var svg = document.createElement("svg");
+    svg.setAttributeNS(null, "height", 50);
+    svg.setAttributeNS(null, "width", 50);
+    var circle = document.createElement("circle");
+    circle.setAttributeNS(null, "cx", 50);
+    circle.setAttributeNS(null, "cy", 50);
+    circle.setAttributeNS(null, "r", 60);
+    circle.setAttributeNS(null, "fill", "red");
+    circle.setAttributeNS(null, "stroke", "red");
+    circle.setAttributeNS(null, "stroke-width", "3");
+    svg.appendChild(circle);
+
+    var currentDiv = document.getElementById("graph");
+    currentDiv.parentNode.insertBefore(svg, currentDiv); 
+
+    svg.width = 100;
+    svg.height = 100;
+    circle.cx = 50;
+    circle.cy = 50;
+    circle.r = 40;
+    circle.fill = "green";
+}
