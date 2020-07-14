c542f09bb1bbb54d35245aa4eb33046c9b9cd815
Abbta <abbe.tanndal@gmail.com>
2020-07-14 20:52:12 +0200

    drawable graph

--- graph.js
+++ graph.js
@@ -1,27 +1,15 @@
-document.onload = function () {
-
-}
+var yValues = [
+50,60,20,190,340,230,70,80
+]
 
 document.body.onload = function () {
-    var svg = document.createElement("svg");
-    svg.setAttributeNS(null, "height", 50);
-    svg.setAttributeNS(null, "width", 50);
-    var circle = document.createElement("circle");
-    circle.setAttributeNS(null, "cx", 50);
-    circle.setAttributeNS(null, "cy", 50);
-    circle.setAttributeNS(null, "r", 60);
-    circle.setAttributeNS(null, "fill", "red");
-    circle.setAttributeNS(null, "stroke", "red");
-    circle.setAttributeNS(null, "stroke-width", "3");
-    svg.appendChild(circle);
-
-    var currentDiv = document.getElementById("graph");
-    currentDiv.parentNode.insertBefore(svg, currentDiv); 
-
-    svg.width = 100;
-    svg.height = 100;
-    circle.cx = 50;
-    circle.cy = 50;
-    circle.r = 40;
-    circle.fill = "green";
+    var svg = document.getElementById("graph");
+    for (var i = 0; i < yValues.length; i++) {
+        var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        circle.setAttributeNS(null, "cx", i * 10);
+        circle.setAttributeNS(null, "cy", yValues[i]);
+        circle.setAttributeNS(null, "r", 2);
+        circle.setAttributeNS(null, "fill", "red");
+        svg.appendChild(circle);
+    }
 }
