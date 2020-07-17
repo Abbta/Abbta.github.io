88236960d061e9e5364d140e59839d4d95178537
Abbta <abbe.tanndal@gmail.com>
2020-07-17 18:13:36 +0200

    graph structure

--- graph.js
+++ graph.js
@@ -1,6 +1,8 @@
 var yValues = [
 -0.02, 0.045, -0.004, -0.01, -0.03, -0.02, -0.06, -0.03, -0.003, -0.01, 0, -0.02, -0.07, -0.02, -0.04, -0.04, -0.03, -0.04, -0.085, -0.014, -0.011, 0.03, -0.05, 0, -0.02
 ]
+var backgroundYValues = [0.15, 0.1, 0.05, -0.05, -0.1, -.15]
+var horisLength = 0.3;
 var dataMargin = 0.05;
 var yDistance = 0.35
 var yMin = -0.2;
@@ -8,6 +10,21 @@ var xMin = 0;
 
 document.body.onload = function () {
     var svg = document.getElementById("graph");
+    initBackground(svg);
+    initData(svg);
+}
+
+function yScale(elem, yValue) {
+    var scalar = elem.scrollHeight / yDistance;
+    return (-yValue - yMin) * scalar;
+}
+
+function xScale(element, xValue) {
+    var scalar = element.clientWidth / yValues.length;
+    return Math.abs(xValue - xMin) * scalar + 5;
+}
+
+function initData(svg) {
     for (var i = 0; i < yValues.length; i++) {
         var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
         circle.setAttributeNS(null, "cx", xScale(svg, i));
@@ -23,16 +40,40 @@ document.body.onload = function () {
         vertical.setAttributeNS(null, "y2", yScale(svg, yValues[i] - dataMargin));
         vertical.setAttributeNS(null, "style", "stroke:rgb(255,0,0);stroke-width:2");
         svg.appendChild(vertical);
+
+        var horisontalTop = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        horisontalTop.setAttributeNS(null, "x1", xScale(svg, i - horisLength));
+        horisontalTop.setAttributeNS(null, "x2", xScale(svg, i + horisLength));
+        horisontalTop.setAttributeNS(null, "y1", yScale(svg, yValues[i] - dataMargin));
+        horisontalTop.setAttributeNS(null, "y2", yScale(svg, yValues[i] - dataMargin));
+        horisontalTop.setAttributeNS(null, "style", "stroke:rgb(255,0,0);stroke-width:2");
+        svg.appendChild(horisontalTop);
+
+        var horisontalBot = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        horisontalBot.setAttributeNS(null, "x1", xScale(svg, i - horisLength));
+        horisontalBot.setAttributeNS(null, "x2", xScale(svg, i + horisLength));
+        horisontalBot.setAttributeNS(null, "y1", yScale(svg, yValues[i] + dataMargin));
+        horisontalBot.setAttributeNS(null, "y2", yScale(svg, yValues[i] + dataMargin));
+        horisontalBot.setAttributeNS(null, "style", "stroke:rgb(255,0,0);stroke-width:2");
+        svg.appendChild(horisontalBot);
     }
 }
 
-function yScale(elem, yValue) {
-    var scalar = elem.scrollHeight / yDistance;
-    console.log(scalar);
-    return (yValue - yMin) * scalar;
-}
-
-function xScale(element, xValue) {
-    var scalar = element.clientWidth / yValues.length;
-    return Math.abs(xValue - xMin) * scalar + 5;
+function initBackground(svg) {
+    var vertical = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    vertical.setAttributeNS(null, "x1", xScale(svg, 0));
+    vertical.setAttributeNS(null, "x2", xScale(svg, yValues.length));
+    vertical.setAttributeNS(null, "y1", yScale(svg, 0));
+    vertical.setAttributeNS(null, "y2", yScale(svg, 0));
+    vertical.setAttributeNS(null, "style", "stroke:rgb(0,0,0);stroke-width:3");
+    svg.appendChild(vertical); 
+    for (var i = 0; i < backgroundYValues.length; i++) {
+        var vertical = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        vertical.setAttributeNS(null, "x1", xScale(svg, 0));
+        vertical.setAttributeNS(null, "x2", xScale(svg, yValues.length));
+        vertical.setAttributeNS(null, "y1", yScale(svg, backgroundYValues[i]));
+        vertical.setAttributeNS(null, "y2", yScale(svg, backgroundYValues[i]));
+        vertical.setAttributeNS(null, "style", "stroke:rgb(150,150,150);stroke-width:2");
+        svg.appendChild(vertical); 
+    }
 }
